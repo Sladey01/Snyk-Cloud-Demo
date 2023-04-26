5b79d9c5d5a76fa198fbde25d4983ef833d93d5e
Sladey01 <103081216+Sladey01@users.noreply.github.com>
2023-04-26 15:26:19 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,14 +4,14 @@
 variable "victim_company" {
   type        = string
   description = "For naming purposes"
-  default     = "testpatch"
+  default     = "hipatch"
 }
 
 #CHANGE THIS
 variable "owner" {
   type        = string
   description = "For Tagging and Filtering purposes"
-  default     = "Patch"
+  default     = "heyPatch"
 }
 
 #AWS Region
